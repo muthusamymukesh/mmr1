d4a4636bc5cf3eab6b55a2f6f883d6efb4520fa6
muthusamymukesh <35652643+muthusamymukesh@users.noreply.github.com>
2018-02-22 16:24:47 +0530

    Create mm44.c

---
+++ mm44.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    
+   int a;
+    printf("Enter the value");
+    scanf("%d",&a);
+    if (a=1 && a<10)
+    {
+    printf("\n yes");
+}
+else
+{
+printf("\n no");
+    
+}}
+
