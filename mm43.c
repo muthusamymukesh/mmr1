314f2f8358d765df0fd69a95b644b4cfae6c560d
muthusamymukesh <35652643+muthusamymukesh@users.noreply.github.com>
2018-02-22 16:04:26 +0530

    Create mm43.c

---
+++ mm43.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    
+    char b[100],a[100];
+    printf("Enter the string 1");
+    scanf("%s\n",&a);
+    printf("\nEnter the string 2");
+    scanf("\n%s",&b);
+    printf("\n%s%s",a,b);
+}
